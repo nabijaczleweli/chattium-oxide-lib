43632905df9972114f2f15b92177e7f9b36e4277
nabijaczleweli <nabijaczleweli@gmail.com>
2015-12-12 01:14:57 +0100

    Add stub root module documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! `chattium-oxide-client` and `chattium-oxide-server` common files
+//!
+//! chattium-oxide-client: https://github.com/nabijaczleweli/chattium-oxide-client
+//! chattium-oxide-server: https://github.com/nabijaczleweli/chattium-oxide-server
+
 extern crate time;
 extern crate serde;
 extern crate serde_json;
