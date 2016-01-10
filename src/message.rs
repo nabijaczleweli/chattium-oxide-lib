b0541bbb6b2cc65c09f03a03fb59561a3f10e027
nabijaczleweli <nabijaczleweli@gmail.com>
2016-01-10 11:14:41 +0100

    Add id to ChatMessage, RwLock-friendly filler

--- src/message.rs
+++ src/message.rs
@@ -1,4 +1,5 @@
 use ChatUser;
+use std::ops::DerefMut;
 use json::{FromJsonnable, ToJsonnable};
 use time::{now_utc, Tm};
 use serde::de::{Error, Type};
@@ -12,6 +13,7 @@ pub struct ChatMessage {
 	pub sender: ChatUser,
 	pub value: String,
 	pub time_posted: Tm,
+	pub id: u64,
 }
 
 
@@ -21,7 +23,14 @@ impl ChatMessage {
 			sender: by,
 			value: contents,
 			time_posted: now_utc(),
+			id: 0
 		}
+	}
+
+	/// Can be used with, say, `Rwlock<u64>.write().unwrap()`
+	pub fn fill_id<IdFiller: DerefMut<Target=u64>>(&mut self, mut curid: IdFiller) {
+		self.id = *curid;
+		*curid += 1;
 	}
 }
 
@@ -32,27 +41,37 @@ impl FromJsonnable for ChatMessage {
 				let sender = try!(
 					match map.get("sender") {
 						Some(sender) => ChatUser::from_json(sender.clone()),
-						None => Err(JsonError::missing_field("Missing \"sender\"")),
+						None         => Err(JsonError::missing_field("Missing \"sender\"")),
 					});
 				let value =
 					match map.get("value") {
 						Some(value) =>
 							match value {
 								&Value::String(ref value) => value,
-								_ => return Err(JsonError::type_mismatch(Type::String)),
+								_                         => return Err(JsonError::type_mismatch(Type::String)),
 							},
 						None => return Err(JsonError::missing_field("Missing \"value\"")),
 					};
 				let time_posted = try!(
 					match map.get("time_posted") {
 						Some(time_posted) => Tm::from_json(time_posted.clone()),
-						None => Err(JsonError::missing_field("Missing \"time_posted\"")),
+						None              => Err(JsonError::missing_field("Missing \"time_posted\"")),
 					});
+				let id =
+					match map.get("id") {
+						Some(id) =>
+							match id {
+								&Value::U64(ref id) => *id,
+								_                   => return Err(JsonError::type_mismatch(Type::U64)),
+							},
+						None => 0,
+					};
 
 				Ok(ChatMessage{
 					sender: sender,
 					value: value.clone(),
 					time_posted: time_posted,
+					id: id,
 				})
 			},
 			_ => Err(JsonError::type_mismatch(Type::Struct)),
@@ -62,9 +81,14 @@ impl FromJsonnable for ChatMessage {
 
 impl ToJsonnable for ChatMessage {
 	fn to_json(&self) -> Value {
-		ObjectBuilder::new().insert("sender", &self.sender.to_json())
-		                    .insert("value", &self.value)
-		                    .insert("time_posted", &self.time_posted.to_json())
-		                    .unwrap()
+		let builder = ObjectBuilder::new().insert("sender"     , &self.sender.to_json())
+		                                  .insert("value"      , &self.value)
+		                                  .insert("time_posted", &self.time_posted.to_json());
+
+		if self.id != 0 {
+			builder.insert("id", &self.id)
+		} else {
+			builder
+		}.unwrap()
 	}
 }
